ec321580b781d44108f585cd562854cc0a79f5ce
liba <libatalukdar@gmail.com>
2025-08-15 15:56:53 +0600

    input and button selected for dom

---
+++ index.js
@@ -0,0 +1,4 @@
+const todoInput = document.getElementById("todoInput");
+    const addBtn = document.getElementById("addBtn");
+    const todoList = document.getElementById("todoList");
+    const message = document.getElementById("message");
